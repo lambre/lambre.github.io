cfd0c11c9bce42cc8f4ae6199b02e55bbfabc2a6
alexochirov <vergil.009@mail.ru>
2018-12-16 09:33:51 +0300

    new bg and files

--- gulpfile.js
+++ gulpfile.js
@@ -1,194 +1,171 @@
 'use strict';
 
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    pug = require('gulp-pug'),
-    autoprefixer = require('gulp-autoprefixer'),
-    useref = require('gulp-useref'),
-    browserSync = require('browser-sync'),
-    gulpif = require('gulp-if'),
-    ignore = require('gulp-ignore'),
-    rimraf = require('gulp-rimraf'),
-    uglify = require('gulp-uglify'),
-    imagemin = require('gulp-imagemin'),
-    pngquant = require('imagemin-pngquant');
+const del = require('del');
+const path = require('path');
+const gulp = require('gulp');
+const gulpSass = require('gulp-sass');
+const gulpPug = require('gulp-pug');
+const gulpAutoprefixer = require('gulp-autoprefixer');
+const gulpBabel = require('gulp-babel');
+const gulpConcat = require('gulp-concat');
+const gulpUglify = require('gulp-uglify');
+const gulpPlumber = require('gulp-plumber');
+const gulpImagemin = require('gulp-imagemin');
+const gulpNewer = require('gulp-newer');
+const gulpArgs = require('yargs').argv;
+const gulpSassGlob = require('gulp-sass-glob');
+const gulpBrowserSync = require('browser-sync');
+const gulpWait = require('gulp-wait2');
+const imageminPngquant = require('imagemin-pngquant');
 
-var sassPaths = [
-    './node_modules/foundation-sites/scss',
-    './node_modules/motion-ui/src',
-    './node_modules/font-awesome/scss/',
-    './node_modules/slick-carousel/slick/'
+let nameChosenPage = gulpArgs.pg || '*';
+
+const sassPaths = [
+  './node_modules/foundation-sites/scss',
+  './node_modules/motion-ui/src',
+  './node_modules/font-awesome/scss/',
+  './node_modules/slick-carousel/slick/'
 ];
-
-var path = {
+const paths = {
     dist: {
-        html: '',
-        js: 'js/',
-        css: 'css/',
-        images: 'images/',
-        i: 'i/',
-        fonts: 'fonts/'
+        html: 'dist/',
+        js: 'dist/js/',
+        css: 'dist/css/',
+        images: 'dist/images/',
+        i: 'dist/i/',
+        fonts: 'dist/fonts/'
     },
     src: {
-        html: 'src/pug/*.pug',
-        js: 'src/pug/**/_js.html',
-        jsHtml: '*.html',
-        css: ['src/scss/style.scss'],//, 'src/scss/style-inline.scss'
+        html: 'src/pug/' + nameChosenPage + '.pug',
+        js: 'src/js/*.js',
+        jsLib: [
+            'node_modules/jquery/dist/jquery.min.js',
+            'node_modules/foundation-sites/dist/foundation.min.js',
+            'node_modules/slick-carousel/slick/slick.min.js',
+            'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.js',
+
+        ],
+        css: ['src/scss/style.scss'],
         images: 'src/images/**/*.*',
         i: 'src/i/**/*.*',
-        fonts: 'src/fonts/**/*.*',
-        awesome: 'node_modules/font-awesome/fonts/**/*.*'
+        fonts: 'src/fonts/**/*.*'
     },
     watch: {
-        html: ['src/pug/**/*.pug', 'src/pug/**/*.html'],//, 'src/scss/**/*.scss'
-        js: ['src/js/**/*.js', 'src/pug/**/_js.html'],
-        jsHtml: ['src/pug/**/*.pug', 'src/pug/**/*.html'],//, 'src/scss/**/*.scss'
-        css: 'src/scss/**/*.scss',
+        html: ['src/pug/**/*.pug'],
+        js: ['src/js/**/*.js'],
+        css:['src/scss/**/*.scss','src/pug/components/blocks/**/*.scss'],
         images: 'src/images/**/*.*',
         i: 'src/i/**/*.*',
-        fonts: 'src/fonts/**/*.*',
-        awesome: 'node_modules/font-awesome/fonts/**/*.*'
+        fonts: 'src/fonts/**/*.*'
     }
 };
 
-gulp.task('clean-css', function() {
-    return gulp.src(path.dist.css, {read: false}).pipe(rimraf());
-});
 
-gulp.task('clean-html', function() {
-    return gulp.src(path.dist.html + '*.html', {read: false}).pipe(rimraf());
-});
+let clean = () => del(paths.dist.html);
+let cleanCss = () => del(paths.dist.css);
+let cleanHtml = () => del(paths.dist.html + '*.html');
+let cleanI = () => del(paths.dist.i);
+let cleanFonts = () => del(paths.dist.fonts);
+let cleanJs = () => del(paths.dist.js);
 
-gulp.task('clean-images', function() {
-    return gulp.src(path.dist.images, {read: false}).pipe(rimraf());
-});
+let sass = () => gulp.src(paths.src.css)
+    .pipe(gulpPlumber())
+    .pipe(gulpSassGlob())
+    .pipe(gulpSass({
+        includePaths: sassPaths,
+        outputStyle: 'compressed'
+    }))
+    .pipe(gulpAutoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(gulp.dest(paths.dist.css));
 
+let pug = () => gulp.src(paths.src.html)
+    .pipe(gulpPlumber())
+    .pipe(gulpPug({
+        pretty: true,
+        cache: true
+    }))
+    .pipe(gulp.dest(paths.dist.html));
 
+let images = () => gulp.src(paths.src.images, {allowEmpty: true})
+    .pipe(gulpNewer(paths.dist.images))
+    .pipe(gulpImagemin({
+        progressive: true,
+        svgoPlugins: [{removeViewBox: false}],
+        use: [imageminPngquant()],
+        interlaced: true
+    }))
+    .pipe(gulp.dest(paths.dist.images))
+    .pipe(gulpWait(100))
+    .pipe(gulpBrowserSync.reload({
+        stream: true
+    }));
 
-gulp.task('clean-fonts', function() {
-    return gulp.src(path.dist.fonts, {read: false}).pipe(rimraf());
-});
+let i = () => gulp.src(paths.src.i, {allowEmpty: true}).pipe(gulp.dest(paths.dist.i));
 
-gulp.task('clean-js', function() {
-    return gulp.src(path.dist.js, {read: false}).pipe(rimraf());
-});
+let fonts = () => gulp.src(paths.src.fonts, {allowEmpty: true}).pipe(gulp.dest(paths.dist.fonts));
 
-gulp.task('clean-js-copy', ['js-copy'], function() {
-    return gulp.src('components', {read: false}).pipe(rimraf());
-});
+let jsLib = () => gulp.src(paths.src.jsLib, {allowEmpty: true})
+    .pipe(gulpUglify())
+    .pipe(gulpConcat('lib.js'))
+    .pipe(gulp.dest(paths.dist.js));
 
-gulp.task('clean-js-minify', ['js-minify'], function() {
-    return gulp.src('components', {read: false}).pipe(rimraf());
-});
+let jsApp = () => gulp.src(paths.src.js, {allowEmpty: true})
+    .pipe(gulpBabel({
+        presets: ['@babel/env']
+    }))
+    .pipe(gulpConcat('app.js'))
+    .pipe(gulp.dest(paths.dist.js));
 
-gulp.task('clean', function() {
-    return gulp.src('', {read: false}).pipe(rimraf());
-});
+let jsAppMinify = () => gulp.src(paths.src.js, {allowEmpty: true})
+    .pipe(gulpBabel({
+        presets: ['@babel/env']
+    }))
+    .pipe(gulpUglify())
+    .pipe(gulpConcat('app.js'))
+    .pipe(gulp.dest(paths.dist.js));
 
-gulp.task('sass', ['clean-css'], function () {
-    return gulp.src(path.src.css)
-        .pipe(sass({
-                includePaths: sassPaths,
-                outputStyle: 'compressed'
-            })
-            .on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions', 'ie >= 9']
-        }))
-        .pipe(gulp.dest(path.dist.css))
-        .pipe(browserSync.reload({ 
-         stream: true
-        }));
-});
+let reloadBrowser = () => gulp.src(paths.src.js, {allowEmpty: true}).pipe(gulpWait(100))
+    .pipe(gulpBrowserSync.reload({
+        stream: true
+    }));
 
-gulp.task('pug', ['clean-html', 'sass'], function() {
-    return gulp.src(path.src.html)
-        .pipe(pug({
-            pretty: true,
-            cache: true
-        }))
-        .pipe(gulp.dest(path.dist.html));
-});
+let watch = () => {
+    gulp.watch(paths.watch.css, gulp.series(cleanCss, sass, reloadBrowser));
+    gulp.watch(paths.watch.html, gulp.series(cleanHtml, pug, reloadBrowser));
+    gulp.watch(paths.watch.i, gulp.series(cleanI, i, reloadBrowser));
+    gulp.watch(paths.watch.fonts, gulp.series(cleanFonts, fonts, reloadBrowser));
+    gulp.watch(paths.watch.js, gulp.series(cleanJs, jsLib, jsApp, reloadBrowser));
 
-gulp.task('pug2', ['pug'], function() {
-    return browserSync.reload({ 
-         stream: true
-        });
-});
+    let imagesWatcher = gulp.watch(paths.watch.images, images);
+    imagesWatcher.on('unlink', (unlinkPath) => {
+        let filePathFromSrc = path.relative(path.resolve('src/images'), unlinkPath);
+        let distFilePath = path.resolve('dist/images', filePathFromSrc);
+        del(distFilePath);
+        console.log("Delete file: " + distFilePath);
+    });
+};
 
-gulp.task('images', ['clean-images'], function () {
-    return gulp.src(path.src.images)
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngquant()],
-            interlaced: true
-        }))
-        .pipe(gulp.dest(path.dist.images))
-        .pipe(browserSync.reload({ 
-         stream: true
-        }));
-});
+let browserSync = () =>
+    gulpBrowserSync.init({
+        server: {
+            baseDir: "dist"
+        }
+    });
 
+gulp.task('default',
+    gulp.series(
+        clean,
+        gulp.parallel(sass, pug, images, i, fonts, jsLib, jsApp),
+        gulp.parallel(watch,browserSync)
+    )
+);
 
-
-gulp.task('fonts', ['clean-fonts'], function() {
-    return gulp.src([path.src.fonts, path.src.awesome])
-        .pipe(gulp.dest(path.dist.fonts))
-        .pipe(browserSync.reload({ 
-         stream: true
-        }));
-});
-
-gulp.task('js-html', ['pug'], function () {
-    return gulp.src(path.src.jsHtml)
-        .pipe(useref({noAssets: true}))
-        .pipe(gulp.dest(path.dist.html))
-        .pipe(browserSync.reload({ 
-         stream: true
-        }));
-});
-
-gulp.task('js-copy', ['clean-js'], function () {
-    return gulp.src(path.src.js)
-        .pipe(useref())
-        .pipe(gulp.dest(path.dist.html))
-        .pipe(browserSync.reload({ 
-         stream: true
-        }));
-});
-
-gulp.task('js-minify', ['clean-js', 'pug'], function () {
-    return gulp.src(path.src.js)
-        .pipe(useref())
-        .pipe(gulpif('*.js', uglify()))
-        .pipe(gulp.dest(path.dist.html));
-});
-
-
-
-
-gulp.task('minify', ['sass', 'pug', 'images', 'fonts', 'js-html', 'js-minify', 'clean-js-minify']);
-
-gulp.task('default', ['browserSync','sass', 'pug', 'images', 'fonts', 'js-html', 'js-copy', 'clean-js-copy'], function () {
-    gulp.watch([path.watch.css], ['sass']);
-    gulp.watch([path.watch.html], ['pug2']);
-    gulp.watch([path.watch.images], ['images']);
-    gulp.watch([path.watch.fonts], ['fonts']);
-    gulp.watch([path.watch.jsHtml], ['js-html']);
-    gulp.watch([path.watch.js], ['js-copy', 'clean-js-copy']);
-});
-// Start browserSync server
-gulp.task('browserSync', function() {
-  browserSync({
-    server: {
-      baseDir: ''
-    }
-  })
-});
-
-//https://gist.github.com/Insayt/272c9b81936a03884768
-//https://gist.github.com/Deraen/9488df411b61fbe6c831
-//https://habrahabr.ru/post/250569/
-//https://blog.engineyard.com/2014/frontend-dependencies-management-part-2
-//https://gist.github.com/dshafik/07dc3985b5f4888865ea
+gulp.task('minify',
+    gulp.series(
+        clean,
+        gulp.parallel(sass, pug, images, i, fonts, jsLib, jsAppMinify)
+    )
+);
--- js/app.js
+++ js/app.js
@@ -1,32 +1,31 @@
+"use strict";
+
 ;
 $(document).foundation();
 
-(function($) {
-    "use strict";
-    $(function() {
+(function ($) {
+  "use strict";
 
-        /**
-         * Разные карусели
-         */
-        $(".x").slick({
-            infinite: true,
-            dots: false,
-            arrows: true,
-            slidesPerRow: 1,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 5000,
-            autoplayHoverPause: true,
-            fade: true,
-            swipeToSlide: true,
-            prevArrow: '<i class="slick-prev fa fa-angle-left fa-3x"> </i>',
-            nextArrow: '<i class="slick-next fa fa-angle-right fa-3x"> </i>'
-        });
-
-        // $(document).ready(function() {
-        //     $(".fancybox").fancybox();
-        // });
-
-    });
+  $(function () {
+    /**
+     * Разные карусели
+     */
+    $(".x").slick({
+      infinite: true,
+      dots: false,
+      arrows: true,
+      slidesPerRow: 1,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      autoplayHoverPause: true,
+      fade: true,
+      swipeToSlide: true,
+      prevArrow: '<i class="slick-prev fa fa-angle-left fa-3x"> </i>',
+      nextArrow: '<i class="slick-next fa fa-angle-right fa-3x"> </i>'
+    }); // $(document).ready(function() {
+    //     $(".fancybox").fancybox();
+    // });
+  });
 })(jQuery);
